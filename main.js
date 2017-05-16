35fa3f6804b1347401d2b9542a4f5a25b86ea69b
Bill Mercer <bmercer1972@yahoo.com>
2017-05-16 15:19:07 -0400

    HW4 Update

---
+++ main.js
@@ -0,0 +1,15 @@
+var givenName;
+console.log(givenName);
+givenName = "Brett";
+console.log(givenName);
+givenName = givenName;
+console.log(givenName);
+var greeting = "Hello, how are you " + givenName + "?";
+console.log(greeting);
+
+var high = 50;
+var low = 10;
+var math = high - low;
+console.log(math);
+math = high / "5";
+console.log(math);
