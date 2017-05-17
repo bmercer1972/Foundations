cfa4d8ea3b9cc5a7cb670cb3ac927a437293ca8b
Bill Mercer <bmercer1972@yahoo.com>
2017-05-17 18:25:59 -0400

    HW5 Update
    Updated JS

--- main.js
+++ main.js
@@ -1,17 +1,43 @@
-//Just testing javascript
+//Exercise 1
+function addNumbers(numberA, numberB) {
+  return (numberA + numberB);
+}
 
-var givenName;
-console.log(givenName);
-givenName = "Brett";
-console.log(givenName);
-givenName = givenName;
-console.log(givenName);
-var greeting = "Hello, how are you " + givenName + "?";
-console.log(greeting);
+var twoPlusTwo = addNumbers(2, 2);
 
-var high = 50;
-var low = 10;
-var math = high - low;
-console.log(math);
-math = high / "5";
-console.log(math);
+console.log(twoPlusTwo); // 4
+
+//Exercise 2
+function yell(myString) {
+  console.log(myString.toUpperCase());
+}
+yell("test");
+
+function yell10(myString) {
+  for(count = 0; count < 10; count++){
+    yell(myString);
+  }
+}
+yell10("test10");
+
+//Exercise 3
+function longest(string1, string2) {
+  if (string1.length > string2.length) {
+    return string1;
+  } else {
+    return string2;
+  }
+}
+console.log(longest("Bill", "Jim"));
+
+//Exercise 4
+function isVowel(myChar){
+  var myUChar = myChar.toUpperCase();
+  if (myUChar === "A" || myUChar === "E" || myUChar === "I" || myUChar === "O" || myUChar === "U") {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isVowel("C"));
+console.log(isVowel("E"));
